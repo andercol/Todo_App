91ea899a6e856708ca40824346e197f612a0e821
Anderson Colin <andercol@gmail.com>
2022-06-13 16:21:08 -0300

    app funcional

---
+++ frontend/src/main/routes.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+import Todo from '../todo/Todo'
+import About from '../about/About'
+
+const AppRoutes = props => (
+        <Routes>
+            <Route path='/todos' element={<Todo />} />
+            <Route path='/about' element={<About />} />
+            <Route path='*' element={<Navigate to="/todos" />} />
+        </Routes>
+)
+
+export default AppRoutes
